6773cce6a7b0e1a78a06be62152a2cf13b1f984c
Seangkhin Khim <khseangkhin@gmail.com>
2024-10-10 18:40:21 +0700

    completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,19 +8,7 @@ module.exports = {
       lg: '992px',
       xl: '1200px'
     },
-    // container: {
-    //   center: true,
-    //   padding: '2rem'
-    // },
     extend: {
-      colors: {
-        bgBlack: '#161616',
-        bgRed: '#fa1939',
-        bgYellow: '#F9A62B',
-        bgPink: '#fa5480',
-        bgGreen: '#38c378',
-        bgBlue: '#4390f8'
-      },
       gridTemplateColumns: {
         '16': 'repeat(auto-fit, minmax(10rem, 1fr))',
       }
